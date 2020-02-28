b6bc79d794d911545edc8fad8bea7dfda228f793
Maximiliano Osorio <maxiosorio@gmail.com>
2020-02-27 19:16:44 -0800

    more docs

--- src/main/resources/servers/python/generate-server.sh
+++ src/main/resources/servers/python/generate-server.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env bash
+set -e
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+WHITE='\033[0;37m'
+RESET='\033[0m'
+
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker is installed"
+else
+    echo "Docker is not installed"
+    exit 1
+fi
+
+
 dir=${PWD}
 SERVER_DIR=server
 docker run -ti --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v4.1.2 \
      generate  \
-     -i /local/model-catalog.yaml\
+     -i /local/openapi.yaml\
      -g python-flask  \
      -o /local/$SERVER_DIR/ \
-     --git-repo-id model-catalog-api \
-     --git-user-id mintproject \
      --template-dir /local/.openapi-generator/template \
      --ignore-file-override /local/.openapi-generator-ignore
-rm -f $SERVER_DIR/openapi_server/controllers/default_controller.py
-cp -rv ${PWD}/.openapi-generator/template/static_files/utils/ ${PWD}/$SERVER_DIR/openapi_server/utils/
-cp -rv ${PWD}/.openapi-generator/template/static_files/settings/ ${PWD}/$SERVER_DIR/openapi_server/settings/
-cp -rv ${PWD}/.openapi-generator/template/static_files/user_controller.py ${PWD}/$SERVER_DIR/openapi_server/controllers/
-cp -rv ${PWD}/.openapi-generator/template/static_files/contexts/ ${PWD}/$SERVER_DIR/contexts/
-cp -rv ${PWD}/.openapi-generator/template/static_files/queries/ ${PWD}/$SERVER_DIR/queries/
+
+cp -r ${PWD}/.openapi-generator/template/static_files/utils/ ${PWD}/$SERVER_DIR/openapi_server/utils/
+cp -r ${PWD}/.openapi-generator/template/static_files/settings/ ${PWD}/$SERVER_DIR/openapi_server/settings/
+cp -r ${PWD}/.openapi-generator/template/static_files/user_controller.py ${PWD}/$SERVER_DIR/openapi_server/controllers/
+mkdir -p ${PWD}/$SERVER_DIR/contexts/
+echo "Copying query files"
+cp -r ../../queries ${PWD}/$SERVER_DIR/queries
+if [ "$?" == "0" ]; then
+        echo -e "${GREEN}SUCCESS${RESET}"
+fi
